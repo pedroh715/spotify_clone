f32ce6b92e6831ac9c7f2b5044a1fa42a1f2ba95
Pedro Grandizoli <pedrohenriquecontato04@gmail.com>
2022-04-09 16:53:33 -0400

    refresh token

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import useAuth from "./useAuth";
+import { Container, Form } from "react-bootstrap";
 
 export default function Dashboard({code}) {
+    const accessToken = useAuth(code)
     return (
-        <div>{code}</div>
+        <Container>{code}</Container>
     );
 }
