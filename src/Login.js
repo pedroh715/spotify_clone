1770c018f75e7992b14c8918165db29a50b5bc0f
Pedro Grandizoli <pedrohenriquecontato04@gmail.com>
2022-04-09 12:59:52 -0400

    login with spotify

---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Container } from 'react-bootstrap'
+
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=e785a4f85953442c96197254d8f1ef6a&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+export default function Login() {
+    return (
+        <Container className="d-flex justify-content-center align-items-center"
+        style={{ minHeight: "100vh" }}>
+            <a className="btn btn-dark btn-lg" href={AUTH_URL}>Login with Spotify</a>
+        </Container>
+    );
+}
